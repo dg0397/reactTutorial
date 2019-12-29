99db2feec93ff66b88d7d5afd57459ecf89c4133
dg0397 <rgg0397@gmail.com>
2019-12-29 08:41:24 -0400

    Updates

---
+++ precache-manifest.6059831c9a4060345c55e8a259f5f804.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b98f3c3b19980ca97983498596985a0",
+    "url": "/dg0397/reactTutorial.git/index.html"
+  },
+  {
+    "revision": "148ef36b09d5edf3b7ec",
+    "url": "/dg0397/reactTutorial.git/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "0074327adc1ebaef46a5",
+    "url": "/dg0397/reactTutorial.git/static/js/2.d99b889c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dg0397/reactTutorial.git/static/js/2.d99b889c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "148ef36b09d5edf3b7ec",
+    "url": "/dg0397/reactTutorial.git/static/js/main.02ab9b1a.chunk.js"
+  },
+  {
+    "revision": "85ce9862351e70821528",
+    "url": "/dg0397/reactTutorial.git/static/js/runtime-main.147d9f4c.js"
+  }
+]);
